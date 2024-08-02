64471ca64fcfdae198df6c063768521e806dc6e5
shoh <behruzrahimov580@gmail.com>
2024-08-02 20:49:45 +0500

    minor fix

--- packages/server/src/app/globals.app.ts
+++ packages/server/src/app/globals.app.ts
@@ -1,19 +1,12 @@
 import { DataSource } from 'typeorm';
 
 import * as entities from '../_db/entity/_index.js';
-import { parseConnectionString } from '../util/utils.js';
 
 import { Config } from './config.app.js';
 
-const dbConnectionOptions = parseConnectionString(Config.db.connection);
-
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: dbConnectionOptions.host,
-  port: dbConnectionOptions.port,
-  username: dbConnectionOptions.username,
-  password: dbConnectionOptions.password,
-  database: Config.db.database,
+  url: Config.db.connection,
   synchronize: true,
   logging: ['error'],
   entities,
