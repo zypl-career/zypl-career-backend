be63e1f910e7aeee240030f3b864c07db723f3ee
shoh <behruzrahimov580@gmail.com>
2024-08-04 17:40:28 +0500

    created dto

--- packages/server/src/dto/_index.ts
+++ packages/server/src/dto/_index.ts
@@ -3,3 +3,4 @@ export * from './university.dto.js';
 export * from './university.dto.js';
 export * from './articles.dto.js';
 export * from './educational-centers.dto.js';
+export * from './specialty.dto.js';
