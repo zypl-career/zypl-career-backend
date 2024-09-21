746bd4034edeba9b3a080b0d94718811b6043d75
axdjuraev <axdjuraev@gmail.com>
2024-09-21 22:51:33 +0500

    remove cors option

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,9 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('Career API')
     .setDescription('The Career API documentation')
