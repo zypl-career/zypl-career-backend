140369aa15030e28a01b68ea3d5176666babcfb3
firdavsurunov <urunovfirdavsjon@gmail.com>
2024-09-21 23:16:03 +0500

    fix(cors): disable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: false,
+  });
   const config = new DocumentBuilder()
     .setTitle('Career API')
     .setDescription('The Career API documentation')
