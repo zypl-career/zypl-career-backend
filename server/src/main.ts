ae795e11f8b37053f2a34e9e3f987ce897d229e2
shoh <behruzrahimov580@gmail.com>
2025-08-12 05:15:29 +0000

    Remove Docker Compose and Nginx configuration files; update server to listen on port 3002 instead of 3000.

--- server/src/main.ts
+++ server/src/main.ts
@@ -16,8 +16,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(3000);
-  console.log('Listening at http://localhost:3000');
+  await app.listen(3002);
+  console.log('Listening at http://localhost:3002');
 }
 
 bootstrap();
